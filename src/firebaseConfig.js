7dabb68de087cc4803973ab64dc7ea9fed91bf8c
osmansufy <47870515+osmansufy@users.noreply.github.com>
2021-05-12 04:25:38 +0600

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,13 +2,7 @@ import firebase from "firebase"
 
 
 const firebaseConfig={
-    apiKey: "AIzaSyDFso3q2ug2WkTJlDLW3jB0EW66gDT8p-Q",
-    authDomain: "solveaz-7cf34.firebaseapp.com",
-    projectId: "solveaz-7cf34",
-    storageBucket: "solveaz-7cf34.appspot.com",
-    messagingSenderId: "889958092755",
-    appId: "1:889958092755:web:47a4976b8b744cc6b5e926",
-    measurementId: "G-1DBHCJY93K"
+ 
 
 
 
